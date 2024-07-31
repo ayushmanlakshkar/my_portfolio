c0bbff4e863c7ea43beb7ab951828051254da950
Ayushman <ayushman.lakshkar7@gmail.com>
2024-07-31 08:52:16 +0530

    made a portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Ayushman Lakshkar </span>
+            from <span className="purple"> Gwalior, India.</span>
+            <br />
+            I am currently pursuing B.Tech in <span className="purple">Artificial Intellignece and Machine Learning</span>
+            <br />
+            
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> BasketBall
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Surfing LinkedIn
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Food
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Talk is cheak show me the code !"{" "}
+          </p>
+          <footer className="blockquote-footer">Ayushman</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
